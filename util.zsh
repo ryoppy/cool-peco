60288078386e8fb1901b2261764e6deec4b49f68
ryoppy <ryoppy0516@gmail.com>
2015-02-28 11:22:49 +0900

    fix for linux

--- util.zsh
+++ util.zsh
@@ -1,12 +1,7 @@
-# zle is active?
-function _zle-is-active() {
-  [[ "$ZLE_STATE" =~ "[a-z]" ]]
-}
-
 # when use keybind, use zle.
 # when use alias, use print.
 function _cool-peco-on-complete() {
-  if [[ _zle-is-active -eq 0 ]]; then
+  if [[ "$ZLE_STATE" =~ "[a-z]" ]]; then
     BUFFER=$1
     CURSOR=$#BUFFER
     zle clear-screen
